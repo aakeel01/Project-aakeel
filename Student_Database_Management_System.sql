45e003bf8f2a9ed6e41ea24000757c163120c9b8
aakeel01 <aakeel@amazon.com>
2024-08-26 15:52:24 +0530

    Update Student_Database_Management_System.sql

--- Student_Database_Management_System.sql
+++ Student_Database_Management_System.sql
@@ -25,21 +25,19 @@ insert into student_table(Student_id,Stu_name,Department ,email_id ,Phone_no,Add
 (6,'Chithra','CS','qrs@gmail.com','4317877 ','qrs.345','2000-10-13','Female','CS','8.8','B'),
 (7,'Minnu','Aeronautical','tuv@gmail.com','4442343','tuv.678','2000-08-18','Female','Aero','9.6','A'),
 (8,'Gautam','Science','wxy@gmail.com','5554448','wxy.911','2000-10-02','Male','Chemisty','8.5','B'),
-(9,'Suarj','Science','zab@gmail.com','5554640','zab.201','2000-10-03','Male','Maths','8.0','C'),
-(10,'Sindhu','Commerce','cde@gmail.com','9984510','cde.202','2000-10-04','Female','Home_science','7.5','C');
+(9,'Suraj','Science','zab@gmail.com','5554640','zab.201','2000-10-03','Male','Maths','8.0','C'),
+(10,'Sindhu','Commerce','cde@gmail.com','9984510','cde.202','2000-10-04','Female','Home_science','4.2','D');
 
-Update student_table
-set Stu_name = 'Suraj'
-where Student_id ='9';
+
 
 /*3. Student Information Retrieval
 Develop a query to retrieve all students' information from the "student_table" and sort them in
 descending order by their grade*/
 
 select* 
-from student_info
-order by grade DESC
-where grade = cast ( grade as decimal);
+from student_table 
+order by grade DESC;
+
 
 /*4. Query for Male Students:
 .Implement a query to retrieve information about all male students from the "student_table."*/
@@ -63,6 +61,9 @@ Write an update statement to modify the email and grade of a student with a spec
 Update student_table
 set Grade = 'B', email_id = 'lmo@gmail.com'
 where Student_id ='9'; 
+
+select *
+from student_table
 
 /*7. Query for Students with Grade "B"
 Develop a query to retrieve the names and ages of all students who have a grade of "B" from
@@ -90,13 +91,8 @@ ALTER TABLE student_table RENAME TO student_info;
 Write a query to retrieve the name of the student with the highest GPA from the
 "student_info" table.*/
 
-select  Stu_name, MAX (gpa)as top_gpa
-from student_info
-group by Stu_name;
 
 SELECT Stu_name, GPA
 FROM student_info
 ORDER BY GPA DESC
 LIMIT 1;
-
-
